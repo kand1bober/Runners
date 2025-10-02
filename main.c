e34dbee9ad41f7eedeefae6fb5ac78f7e010a59d
kand1bober <kurnevich.slava@gmail.com>
2025-10-02 19:21:13 +0300

    meeting of runners works

--- main.c
+++ main.c
@@ -6,24 +6,31 @@
 #include <errno.h>
 #include <sys/ipc.h>
 
-#define RUNNER_MSG_TYPE 7
+// #define RUNNER_MSG_TYPE 7
+#define MSG_SIZE 100
+
+struct msgbuf {
+    long mtype; /* message type, must be > 0 */
+    char mtext[MSG_SIZE]; /* message data */
+};
 
 void judge(int runners_num, int msg_id);
 void runner(int runners_num, int self_num, int msg_id);
 
-void get_msg(int qid, int msg_type, struct msgbuf* msg);
+int get_msg(int qid, int msg_type, struct msgbuf* msg);
 void send_msg(int qid, int msg_type, char* text);
-
-#define MSG_SIZE 5
-
-struct msgbuf {
-    long mtype;       /* message type, must be > 0 */
-    char mtext[MSG_SIZE];    /* message data */
-};
 
 int main(int argc, char* argv[])
 {
-    int runners_num = atoi(argv[1]);
+    int runners_num = 0;
+
+    if (argv[1] != NULL){
+        runners_num = atoi(argv[1]);
+    }       
+    else {
+        printf("no arguments provided\n");
+        exit(0);
+    }
 
     int qid = msgget(IPC_PRIVATE, IPC_CREAT | 0666); //create msg_queue
     if (qid == -1)
@@ -32,8 +39,8 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-
     pid_t pid;
+
     pid = fork();
     if (pid < 0)
     {
@@ -46,7 +53,7 @@ int main(int argc, char* argv[])
         exit(0);
     }
 
-    for (int i = 0; i < runners_num; i++)
+    for (int i = 1; i < runners_num + 1; i++)
     {
         pid = fork();
         if (pid < 0)
@@ -64,8 +71,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i < runners_num + 1; i++)
         wait(NULL);
 
-    if (msgctl(qid, IPC_RMID, 0) == -1)
-    {
+    if (msgctl(qid, IPC_RMID, 0) == -1){
         perror("error in removing msg queue");
         exit(1);
     }
@@ -80,13 +86,19 @@ void judge(int runners_num, int qid)
 
     struct msgbuf msg;
 
-    for (int i = 0; i < runners_num; i++)
+    int runners_count = 0;
+    while (runners_count < runners_num)
     {
-        get_msg(qid, -runners_num, &msg);
-
-        printf("Judge: I met runner #%s \n", msg.mtext);
+        if (get_msg(qid, -runners_num, &msg) == -1)
+        {
+            continue;
+        }
+        else 
+        {
+            printf("Judge: I met runner #%s \n", msg.mtext);
+            runners_count += 1;
+        }
     }
-
 }
 
 
@@ -96,13 +108,13 @@ void runner(int runners_num, int self_num, int qid)
     
     //number of runner in text
     char text[MSG_SIZE];
-    snprintf(text, MSG_SIZE, "%d", self_num);
+    snprintf(text, MSG_SIZE, "%d", self_num); //send messaage with his self number
 
     send_msg(qid, self_num, text);
 }
 
 
-void get_msg(int qid, int msg_type, struct msgbuf* msg)
+int get_msg(int qid, int msg_type, struct msgbuf* msg)
 {
     if (msgrcv(qid, msg, sizeof(msg->mtext), msg_type,
         MSG_NOERROR | IPC_NOWAIT) == -1) 
@@ -113,12 +125,11 @@ void get_msg(int qid, int msg_type, struct msgbuf* msg)
             exit(1);
         }
 
+        return -1; //no messages
         // printf("No message available for msgrcv()\n");
     } 
-    // else 
-    // {
-    //     printf("%s\n", msg.mtext);
-    // }
+
+    return 0;
 }
 
 
